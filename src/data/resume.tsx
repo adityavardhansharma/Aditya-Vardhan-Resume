03d2e3953e26cfba7357a565fa57e4adc8c714c4
Aditya Vardhan Sharma <adityaradhika13@gmail.com>
2024-10-26 01:26:01 +0530

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -250,7 +250,7 @@ export const DATA = {
           icon: <Icons.globe className="size-3" />,
         },
       ],
-      image: "/trustnet.png",
+      image: "/meetroom.png",
       video:
         "",
     },
